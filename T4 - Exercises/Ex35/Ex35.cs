cd4219870e9576ae1d66457af24770cb3a59a210
Laura Zaporta <laura.zaporta.7e8@itb.cat>
2025-02-20 06:56:09 +0100

    35

--- T4 - Exercises/Ex35/Ex35.cs
+++ T4 - Exercises/Ex35/Ex35.cs
@@ -8,11 +8,24 @@ namespace EX35
     {
         public static void Main()
         {
-            Llibre TSHOEH = new Llibre { Title = "The Seven Husbands Of Evelyn Hugo", Author = "Taylor", Year = 2018};
-            Llibre MCom = new Llibre { Title = "Manifest Comunista", Author = "Marx i Engels", Year = 1848};
+            const string path = @"C:\Users\msi 2.0\Source\Repos\T4-EX\T4 - Exercises\Ex35\llibres.json";
 
-            string jsonOne = JsonConvert.SerializeObject(TSHOEH);
-            Console.WriteLine(jsonOne);
+            List<Llibre> nousLlibres = new List<Llibre> {
+                new Llibre { Title = "The Seven Husbands Of Evelyn Hugo", Author = "Taylor", Year = 2018},
+                new Llibre { Title = "Manifest Comunista", Author = "Marx i Engels", Year = 1848}
+            };
+
+            string jsonOne = JsonConvert.SerializeObject(nousLlibres);
+
+            File.WriteAllText(path, jsonOne);
+            string llegirJson = File.ReadAllText(path);
+            List<Llibre> llibresJson = JsonConvert.DeserializeObject<List<Llibre>>(llegirJson);
+            // Amb aquesta instrucció es transforma el json de la ruta d'accès especificada en el format de la llista indicada
+
+            foreach (var llibre in llibresJson)
+            {
+                Console.WriteLine($"Títol: {llibre.Title}, Autor: {llibre.Author}, Any: {llibre.Year}");
+            }
         }
     }
 }
